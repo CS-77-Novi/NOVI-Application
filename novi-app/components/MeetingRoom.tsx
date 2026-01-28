de1631f1799ce0078ee6b75eea56caf48ea31750
Chathila_Wijesinghe <chathilawijesinghe@gmail.com>
2026-01-28 20:45:04 +0530

    Fixed errors and added new styles
    errors fixed:
    -Changed the order of Hooks caled by MeetingRoom.tsx.
    
    styles added:
    -Added meeting status button & show participant's button styles.

--- novi-app/components/MeetingRoom.tsx
+++ novi-app/components/MeetingRoom.tsx
@@ -15,9 +15,8 @@ import { toast } from "sonner";
 import { cn } from "@/lib/utils";
 
 
-import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
 // Dropdown menu components
-import { DropdownMenu, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { LayoutList, Users } from "lucide-react";
 import EndCallButton from "./EndCallButton";
 
@@ -27,6 +26,12 @@ type CallLayoutType = 'grid' | 'speaker-left' |'speaker-right';
 const MeetingRoom =() => {
      // State to control call layout
     const [layout,setLayout]=useState<CallLayoutType>('speaker-left');
+    // State to toggle participants list
+    const [showParticipants, setShowParticipants] = useState(false);
+    // Router for navigation
+    const router = useRouter();
+    // Get current path (used for invite link)
+    const pathname = usePathname();
     // Get logged-in user
     const {user} =useUser();
     if(!user) return
@@ -35,12 +40,7 @@ const MeetingRoom =() => {
     const callingState =useCallCallingState()
     // Show loading until user joins the call
     if (callingState !==CallingState.JOINED) return <Loading />;
-    // State to toggle participants list
-    const[showParticipants,setShowParticipants]= useState(false);
-    // Router for navigation
-    const router = useRouter();
-    // Get current path (used for invite link)
-    const pathname = usePathname();
+    
      // Component to render layout based on selected type
     const CallLayout = () =>{
         switch(layout) {
@@ -57,7 +57,7 @@ const MeetingRoom =() => {
        <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
         <Button className='ml-5 font-semibold bg-gray-900 hover:scale-110 rounded-3x1'
             onClick={() =>{
-                const meetingLink = '$(process.env.NEXT_PUBLIC_BASE_URL)${pathname}'
+                const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`
                 navigator.clipboard.writeText(meetingLink);
                 toast('Meeting Link Copied',{
                     duration: 3000,
@@ -66,54 +66,58 @@ const MeetingRoom =() => {
             }}
             >
                 Invite People
-            </Button>
-            <div className="relative flex size-full items-center justify-center">
-                <div className="flex size-full max-w-[1000px] items-center animate-fade-in">
-                <CallLayout/>
-                </div> 
-                <div
-                className={cn('h-[calc(100vh-86px)] hidden ml-2',{
-                    'show-block' :showParticipants,
-                })}
-                >
-                <CallParticipantsList onClose={() => setShowParticipants(false)} />
-                </div>
-                </div>
-                {/* call controls*/}
-                <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
-                    <CallControls onLeave={() => router.push('/')} />
+        </Button>
 
-                    <DropdownMenu>
+            <div className="relative flex size-full items-center justify-center">
+                <div className=" flex size-full max-w-[1000px] items-center animate-fade-in">
+                    <CallLayout/>
+                </div>
+                {/* Participants panel container */}
+                <div
+                    className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+                        'show-block': showParticipants,
+                    })}
+                    >
+                    <CallParticipantsList onClose={() => setShowParticipants(false)} />
+                </div>
+            </div>
+
+            {/* call controls*/}
+            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+                <CallControls onLeave={() => router.push(`/`)} />
+
+                <DropdownMenu>
                     <div className="flex items-center">
-                        <DropdownMenuTrigger className="cursor-pointer rounded-2x1 bg-[#19232d] px">
-                        <LayoutList size={20} className="text-white"/>
+                        <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+                            <LayoutList size={20} className="text-white" />
                         </DropdownMenuTrigger>
-                        </div> 
-                        <DropdownMenuContent className="border-black text-white">
-                            {['Grid','Speaker-Left','Speaker-Right'].map((item,index) =>(
-                            <div key={index}>
-                                <DropdownMenuItem
+                    </div>
+                    <DropdownMenuContent className="border-black bg-black text-white">
+                        {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                        <div key={index}>
+                            <DropdownMenuItem
                                 onClick={() =>
-                                    setLayout(item.toLowerCase()as CallLayoutType)
-                                }
-                                >
-                                    {item}
-                                </DropdownMenuItem>
-                                <DropdownMenuSeparator className="border-black"/>
-                            </div> 
-                               ))}   
-                               </DropdownMenuContent>   
-                        </DropdownMenu>
-                        <CallStatsButton/>
-                        <button onClick={() => setShowParticipants((prev) => !prev)}>
-                        <div className="cursor-pointer rounded-2x1 bg-[#19232d] px-4 py-2 hover:bg-gray">
-                            <Users size={20} className="text-white"/>
-                            </div>
-                            </button> 
-                            <EndCallButton/>   
-                        </div>     
-                     </section>
-                 )
+                                setLayout(item.toLowerCase() as CallLayoutType)}
+                            >
+                                {item}
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator className="border-black" />
+                        </div>
+                        ))}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+                <CallStatsButton />
+                {/*show participant's button function*/}
+                <button onClick={() => setShowParticipants((prev) => !prev)}> 
+                    <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+                        <Users size={20} className="text-white" />
+                    </div>
+                </button>
+                <EndCallButton />   
+            </div>     
+        </section>
+    )
 
 
 }
--- novi-app/hooks/useGetCallById.tsx
+++ novi-app/hooks/useGetCallById.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useEffect, useState } from "react";
 
