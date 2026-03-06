6dce42ea797d6c79a83cdec5f42a709c228d2cd4
Onaya Sendini <onayasendini06@gmail.com>
2026-03-06 18:57:45 +0530

    implemented quiz card component

--- novi-app/app/layout.tsx
+++ novi-app/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import "react-datepicker/dist/react-datepicker.css";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";                                     
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
