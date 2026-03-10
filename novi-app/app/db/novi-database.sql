0291f9cb707ccf79361bae9a0ef9a765dd8623b0
Sethumdee Narampanawa <narampanawa2006@gmail.com>
2026-03-10 14:35:57 +0530

    Created attention log table
    Records per-second attention scores for each participant during a session.
      Linked to both study_session and participant for detailed analysis.

--- novi-app/app/db/novi-database.sql
+++ novi-app/app/db/novi-database.sql
@@ -19,3 +19,20 @@ CREATE TABLE group_session (
     average_attention_score DECIMAL(5,2),
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
+
+--attention_log table
+CREATE TABLE attention_log (
+    log_id BIGSERIAL PRIMARY KEY,
+    session_id BIGINT NOT NULL,
+    participant_id BIGINT,
+    time_stamp INT NOT NULL,
+    attention_score DECIMAL(5,2) NOT NULL,
+
+    FOREIGN KEY (session_id)
+        REFERENCES study_session(session_id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (participant_id)
+        REFERENCES participant(participant_id)
+        ON DELETE CASCADE
+);
