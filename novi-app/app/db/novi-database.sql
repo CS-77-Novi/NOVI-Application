4bf9ea7e3887c14b1a2e7397a28fa8b6771c49ad
Sethumdee Narampanawa <narampanawa2006@gmail.com>
2026-03-10 14:44:41 +0530

    Deleted the attention log table

--- novi-app/app/db/novi-database.sql
+++ novi-app/app/db/novi-database.sql
@@ -34,23 +34,6 @@ CREATE TABLE group_session (
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
---attention_log table
-CREATE TABLE attention_log (
-    log_id BIGSERIAL PRIMARY KEY,
-    session_id BIGINT NOT NULL,
-    participant_id BIGINT,
-    time_stamp INT NOT NULL,
-    attention_score DECIMAL(5,2) NOT NULL,
-
-    FOREIGN KEY (session_id)
-        REFERENCES study_session(session_id)
-        ON DELETE CASCADE,
-
-    FOREIGN KEY (participant_id)
-        REFERENCES participant(participant_id)
-        ON DELETE CASCADE
-);
-
 --Report table
 CREATE TABLE report (
     report_id BIGSERIAL PRIMARY KEY,
