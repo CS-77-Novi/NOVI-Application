04f28da387061dad29e1b799017031e008606e1d
Abdullah20230909 <mohamed.20230909@iit.ac.lk>
2026-03-22 02:36:54 +0530

    Update  novi-app/app/(main)/(home)/reports/page.tsx
    heading text changes

--- novi-app/app/(main)/(home)/reports/page.tsx
+++ novi-app/app/(main)/(home)/reports/page.tsx
@@ -23,10 +23,8 @@ const ReportsPage = () => {
         
                 </div>
                 
-                <h2 className="text-[15px] font-medium text-center mb-8 leading-[1.6] text-zinc-100">
-                    Are you trying to access the<br />
-                    report? Tell us<br />
-                    who you are.
+                <h2 className="text-[16px] font-semibold text-center mb-8 leading-[1.6] text-gray-800 dark:text-zinc-100 transition-colors duration-300">
+                    Choose your account type<br />to access the reports.
                 </h2>
 
                 <div className="flex flex-col w-full gap-4 px-1">
